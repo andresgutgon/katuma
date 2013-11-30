0701085f9f62b636b59c0c8e285bd7ff89b11a5f
enricostano <ocirne.onats@gmail.com>
2013-11-30 17:38:30 +0100

    Added ActionMailer configs for dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,8 +14,19 @@ Katuma::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  # config.action_mailer.raise_delivery_errors = false
+
+  # Configure ActionMailer
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.katuma.org',
+    port:                 578,
+    domain:               'katuma.org',
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: false
+  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
